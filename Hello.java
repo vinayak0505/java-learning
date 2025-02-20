17a2d0d7ede7d883a9bda4472309ab6f6fa86fbc
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2025-02-20 14:46:21 +0530

    create question game

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
-    public static void main(String a[]){
-        System.out.println("Hello, World!");
+    public static void main(String a[]) {
+        int nums[][] = new int[3][4];
     }
+
+    public static void main(int a[]) {
+        int nums[][] = new int[3][4];
+    }
+}
+
+class Test{
+    int name;
+    int data;
+    
 }
